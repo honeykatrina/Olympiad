6f634b1f01b078d3e32c95cda045ba6e2edebf75
IlyaBkmz <chuykovilya2015@gmail.com>
2018-05-01 16:19:20 +0300

    Project was created

---
+++ Olympiad/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Olympiad
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
