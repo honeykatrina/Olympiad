f2bed84255312bbad6c36201e535616073dc82ac
Лфен-ПК\Лфен <kathaina.morozova@gmail.com>
2018-05-17 09:42:28 +0300

    Start registration in logic layer

---
+++ LogicLayer/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using LogicLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.Interfaces
+{
+    public interface IUserService : IDisposable
+    {
+        Task<ClaimsIdentity> Authenticate(UserDTO userDto);
+        Task SetInitialData(UserDTO adminDto, List<string> roles);
+    }
+}
